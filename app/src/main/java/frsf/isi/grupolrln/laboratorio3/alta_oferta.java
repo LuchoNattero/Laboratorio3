4669ac32eacc39f14387dc3dacfb201f667cfd03
LuchoNattero <32514864+LuchoNattero@users.noreply.github.com>
2017-10-23 10:49:06 -0300

    Initial commit

---
+++ app/src/main/java/frsf/isi/grupolrln/laboratorio3/alta_oferta.java
@@ -0,0 +1,13 @@
+package frsf.isi.grupolrln.laboratorio3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alta_oferta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_oferta);
+    }
+}
