388ebef0e63085752b26176ddc1fb5f239469ab1
LuchoNattero <32514864+LuchoNattero@users.noreply.github.com>
2017-10-25 09:43:19 -0300

    correccion de errores

--- app/src/main/java/frsf/isi/grupolrln/laboratorio3/alta_oferta.java
+++ app/src/main/java/frsf/isi/grupolrln/laboratorio3/alta_oferta.java
@@ -2,6 +2,7 @@ package frsf.isi.grupolrln.laboratorio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class alta_oferta extends AppCompatActivity {
@@ -13,9 +14,9 @@ public class alta_oferta extends AppCompatActivity {
         setContentView(R.layout.activity_alta_oferta);
 
         sp_monedas = (Spinner) findViewById(R.id.sp_moneda);
-
-
-
+        ArrayAdapter adp_array = ArrayAdapter.createFromResource(this,R.array.tipo_moneda,android.R.layout.simple_spinner_item);
+        adp_array.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_monedas.setAdapter(adp_array);
 
     }
 }
